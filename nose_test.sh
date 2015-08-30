a846acdf8b72a895070ce1060dae992e01915691
Francesc Elies Henar <felies@visualfabriq.com>
2015-08-30 14:34:15 +0200

    print versions at the end

--- nose_test.sh
+++ nose_test.sh
@@ -1,4 +1,4 @@
-python -c "import coverage; print 'coverage = ', coverage.__version__"
-python -c "import cython; print 'cython =', cython.__version__"
 python setup.py build_ext -i
 nosetests -vs --with-coverage  tests/test_nose.py
+python -c "import coverage; print 'coverage = ', coverage.__version__"
+python -c "import cython; print 'cython =', cython.__version__"
